7062f4b6f0d743c047c6734bcd54654d86612511
kevinmaddy-git <kevbenefitsyou@gmail.com>
2023-08-22 21:42:56 -0400

    Added in assets and fonts.  Prepare for styling

--- maddy-family-fun/src/components/Navbar/navbar.js
+++ maddy-family-fun/src/components/Navbar/navbar.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "../Navbar/navbar.scss";
+import LogoVideo from '../../assets/images/Logo.mp4';
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="logo">
-        {}
-        <img src="logo.png" alt="Logo" />
-      </div>
-      <div className="nav-links">
-        <Link to="/home" className="nav-link">Home</Link>
-        <Link to="/about" className="nav-link">About</Link>
-        <Link to="/video" className="nav-link">Videos</Link>
-        <Link to="/shop" className="nav-link">Shop</Link>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <video autoPlay muted>
+                    <source src={LogoVideo} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            <div className="nav-links">
+                <Link to="/home" className="nav-link">Home</Link>
+                <Link to="/about" className="nav-link">About</Link>
+                <Link to="/video" className="nav-link">Videos</Link>
+                <Link to="/shop" className="nav-link">Shop</Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
